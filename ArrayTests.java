1b7d7051e150db4fe6643d77504fa76bd6922e40
MoisesRM32 <141669746+MoisesRM32@users.noreply.github.com>
2024-01-31 13:53:13 -0800

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -8,6 +8,12 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
+  @Test
+  public void testReverseInPlaceWithValues(){
+    int[] Vals = {1, 2, 3, 4, 5};
+    ArrayExamples.reverseInPlace(Vals);
+    assertArrayEquals(new int[]{2, 3, 4, 5, 1}, Vals);
+  }
 
 
   @Test
@@ -15,4 +21,15 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedWithRandomValues(){
+    int[] randomlist = {2 ,4 ,6 ,8, 10};
+    assertArrayEquals(new int[]{10, 8, 6, 4, 2}, ArrayExamples.reversed(randomlist));
+  }
+
+
+
+
+
 }
